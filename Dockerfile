4d73970ecd2abcec18923b03141df5216ff6afff
Piotr Marcinkowski <marcinkowski@mailbox.org>
2016-07-09 23:09:14 +0200

    cleanup, add docker compose, add caddy, talk to google-spreadsheet

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,18 @@
-FROM node:wheezy
+FROM node:latest
+
+RUN npm install -g pm2
 
 # Create app directory
 RUN mkdir -p /usr/src/app
 WORKDIR /usr/src/app
 
 # Install app dependencies
-COPY package.json /usr/src/app/
+ADD package.json /usr/src/app/
 RUN npm install
 
 # Bundle app source
-COPY . /usr/src/app
+ADD . /usr/src/app
 
 EXPOSE 3000
-CMD [ "npm", "start" ]
+CMD ["npm", "start"]
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var creds = require('../config/happy-hours-9fc0a1813103.json');
+var GoogleSpreadsheet = require('google-spreadsheet');
+var async = require('async');
+var doc = new GoogleSpreadsheet('1UgUUn3kziCTMvSV5-pORg2tWb4baEec6otytUN-GtB8');
+var sheet;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  async.series([
+    function setAuth(step) {
+      doc.useServiceAccountAuth(creds, step);
+    },
+    function getInfoAndWorksheets(step) {
+      doc.getInfo(function(err, info) {
+        console.log('Loaded doc: '+info.title+' by '+info.author.email);
+        sheet = info.worksheets[0];
+        console.log('sheet 1: '+sheet.title+' '+sheet.rowCount+'x'+sheet.colCount);
+        res.send('sheet 1: '+sheet.title+' '+sheet.rowCount+'x'+sheet.colCount);
+        step();
+      });
+    }
+  ]);
+  // res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
