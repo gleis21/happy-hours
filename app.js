23c5110dbbb2f741884b2919224bdf36a006e7f5
Piotr Marcinkowski <marcinkowski@mailbox.org>
2016-07-30 22:52:34 +0200

    secure cookie

--- app.js
+++ app.js
@@ -76,7 +76,7 @@ app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
-  cookie: {secure: process.env.SESSION_COOKIE_SEC ? true : false}
+  cookie: {}
 }));
 
 // Initialize Passport and restore authentication state, if any, from the
